7c50c3df1e1271ac94643fc4084b1e4c01171d60
pedrolopez <pedrolopez@10.201.1.61>
2023-09-25 21:53:46 +0200

    deveops Person and GenericMethods<T, S, U> classes;

--- src/n1Exe2/GenericMethods.java
+++ src/n1Exe2/GenericMethods.java
@@ -1,5 +1,9 @@
 package n1Exe2;
 
-public class GenericMethods {
+public class GenericMethods<T, S, U> {
+	
+	public void print(T t, S s, U u) {
+		System.out.println("args: " + t + s + u);
+	}
 
 }
