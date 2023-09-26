22464511626623d9b1c53b3e244c95ee7629b742
pedrolopez <pedrolopez@10.201.1.61>
2023-09-26 09:13:41 +0200

    updates App for the n2exe1 package

---
+++ src/n2exe2/App.java
@@ -0,0 +1,17 @@
+package n2exe2;
+
+public class App {
+
+	public static void main(String[] args) {
+		GenericMethods<String, Integer> identityStack = new GenericMethods<>();
+		identityStack.print("adress", 26587, new Person("name", "surname", 9));
+		
+		GenericMethods<String, String> friendsPhoto = new GenericMethods<>();
+		friendsPhoto.print("Flocky", "Brutus", new Person("Petra", "Petrica", 2));
+		
+		GenericMethods<Integer, Integer> beingDigital = new GenericMethods<>();
+		beingDigital.print(100111, 100010, new Person("Nic", "Neg",  612));
+		
+	}
+
+}
