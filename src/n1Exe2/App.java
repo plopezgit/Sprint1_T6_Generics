3622ed4844d61883156ef11869f163b8ad12ac4b
pedrolopez <pedrolopez@10.201.1.61>
2023-09-26 09:29:18 +0200

    move the type argument declaratio to the method level based on strict requirement

--- src/n1Exe2/App.java
+++ src/n1Exe2/App.java
@@ -3,13 +3,13 @@ package n1Exe2;
 public class App {
 
 	public static void main(String[] args) {
-		GenericMethods<Person, String, Integer> identityStack = new GenericMethods<>();
+		GenericMethods identityStack = new GenericMethods();
 		identityStack.print(new Person("name", "surname", 9), "adress", 26587);
 		
-		GenericMethods<String, String, String> friendsPhoto = new GenericMethods<>();
+		GenericMethods friendsPhoto = new GenericMethods();
 		friendsPhoto.print("Flocky", "Brutus", "Petra");
 		
-		GenericMethods<Integer, Integer, Integer> beingDigital = new GenericMethods<>();
+		GenericMethods beingDigital = new GenericMethods();
 		beingDigital.print(100111, 100010, 110001);
 	}
 
--- src/n1Exe2/GenericMethods.java
+++ src/n1Exe2/GenericMethods.java
@@ -1,8 +1,8 @@
 package n1Exe2;
 
-public class GenericMethods<T, S, U> {
+public class GenericMethods {
 	
-	public void print(T t, S s, U u) {
+	public <T, S, U> void print(T t, S s, U u) {
 		System.out.println("args: " + t + s + u);
 	}
 
