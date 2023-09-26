e6547896a35c667483d99e6499d3a7a431a05057
pedrolopez <pedrolopez@10.201.1.61>
2023-09-26 09:21:14 +0200

    updates n2Exe1 package based on requirement guidelines

--- src/n2Exe1/App.java
+++ src/n2Exe1/App.java
@@ -1,16 +1,19 @@
 package n2Exe1;
 
+import n2exe2.GenericMethods;
+import n2exe2.Person;
+
 public class App {
 
 	public static void main(String[] args) {
-		GenericMethods<Person, String, Integer> identityStack = new GenericMethods<>();
-		identityStack.print(new Person("name", "surname", 9), "adress", 26587);
+		GenericMethods<String, Integer> identityStack = new GenericMethods<>();
+		identityStack.print("adress", 26587, new Person("name", "surname", 9));
 		
-		GenericMethods<String, String, String> friendsPhoto = new GenericMethods<>();
-		friendsPhoto.print("Flocky", "Brutus", "Petra");
+		GenericMethods<String, String> friendsPhoto = new GenericMethods<>();
+		friendsPhoto.print("Flocky", "Brutus", new Person("Petra", "Petrica", 2));
 		
-		GenericMethods<Integer, Integer, Integer> beingDigital = new GenericMethods<>();
-		beingDigital.print(100111, 100010, 110001);
+		GenericMethods<Integer, Integer> beingDigital = new GenericMethods<>();
+		beingDigital.print(100111, 100010, new Person("Nic", "Neg",  110001));
 		
 	}
 
---
+++ src/n2Exe1/GenericMethods.java
@@ -0,0 +1,16 @@
+package n2Exe1;
+
+import n2exe2.Person;
+
+public class GenericMethods<T, S> {
+	
+	public void print(T t, S s, Person person) {
+		System.out.println("args: " + t + s + person);
+	}
+	
+	/*
+	 * Modifica el apartado anterior de forma que los argumentos 
+	 * del método genérico sean una lista de argumentos de variable indefinida.
+	 */
+
+}
