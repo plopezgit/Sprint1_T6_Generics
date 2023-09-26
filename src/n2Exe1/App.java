488dcca4c6818639faf14d60d2227c38e23e2874
pedrolopez <pedrolopez@10.201.1.61>
2023-09-26 09:12:00 +0200

    udpates GenericMethods GenericMethods<T, S> class and it method public void print(T t, S s, Person person) base on requirement guideline

---
+++ src/n2Exe1/App.java
@@ -0,0 +1,17 @@
+package n2Exe1;
+
+public class App {
+
+	public static void main(String[] args) {
+		GenericMethods<Person, String, Integer> identityStack = new GenericMethods<>();
+		identityStack.print(new Person("name", "surname", 9), "adress", 26587);
+		
+		GenericMethods<String, String, String> friendsPhoto = new GenericMethods<>();
+		friendsPhoto.print("Flocky", "Brutus", "Petra");
+		
+		GenericMethods<Integer, Integer, Integer> beingDigital = new GenericMethods<>();
+		beingDigital.print(100111, 100010, 110001);
+		
+	}
+
+}
---
+++ src/n2exe2/Person.java
@@ -0,0 +1,45 @@
+package n2exe2;
+
+public class Person {
+	
+	private String personName;
+	private  String personSurName;
+	private int personAge;
+	
+	public Person(String personName, String personSurName, int personAge) {
+		this.personName = personName;
+		this.personSurName = personSurName;
+		this.personAge = personAge;
+	}
+
+	public String getPersonName() {
+		return personName;
+	}
+
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+
+	public String getPersonSurName() {
+		return personSurName;
+	}
+
+	public void setPersonSurName(String personSurName) {
+		this.personSurName = personSurName;
+	}
+
+	public int getPersonAge() {
+		return personAge;
+	}
+
+	public void setPersonAge(int personAge) {
+		this.personAge = personAge;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [Name: " + personName + ", Surname: " + personSurName + ", Age: " + personAge
+				+ "]";
+	}
+
+}
