15a0d13b81dd8bdc8ee4cf568ed5d9e88b74f405
pedrolopez <p.lopez.ch.hr@hotmail.com>
2023-09-27 10:14:47 +0200

    updates xtra.rawTypeTest pkg

--- src/xtra/rawTypeTest/App.java
+++ src/xtra/rawTypeTest/App.java
@@ -5,8 +5,18 @@ public class App {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		@SuppressWarnings("rawtypes")
-		GenericMethods rawIdentityStack = new GenericMethods();
+		GenericClass rawIdentityStack = new GenericClass();
 		rawIdentityStack.print("name");
+		
+		/*The term "unchecked" means that the compiler 
+		 * does not have enough type information to perform all type checks necessary 
+		 * to ensure type safety. 
+		 * 
+		 * The @SuppressWarnings("unchecked") annotation suppresses unchecked warnings. 
+		 * If you are unfamiliar with the @SuppressWarnings syntax, see Annotations.
+		 * (S1T7) 
+		 *
+		 */
 
 	}
 
---
+++ src/xtra/rawTypeTest/GenericClass.java
@@ -0,0 +1,8 @@
+package xtra.rawTypeTest;
+
+public class GenericClass<T> {
+	
+	public void print(T t) {
+		System.out.println("args: " + t);
+	}
+}
